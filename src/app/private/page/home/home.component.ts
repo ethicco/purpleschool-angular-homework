42cfb186b9fe884bec0c628a4ef4c81c3d4208d3
Dmitry Podkopaev <dmitry.podkopaev@ladadigit.io>
2026-05-12 15:22:18 +0300

    add home page

--- src/app/private/page/home/home.component.ts
+++ src/app/private/page/home/home.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IMovie } from '../../../shared/models/movie.model';
+import { MOVIES } from '../../../shared/const/movies.const';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
+  imports: [CardComponent],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  movies: IMovie[] = MOVIES;
+
+  @Input() search = 'test';
+
+  onFavoriteChange(id: string) {
+    this.movies = this.movies.map(movie => {
+      if (movie.id === id) {
+        return {
+          ...movie,
+          isFavorite: !movie.isFavorite,
+        };
+      } else {
+        return movie;
+      }
+    });
+  }
+}
